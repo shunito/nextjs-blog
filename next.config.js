66a4bf305f7171547b5f7a983888f38ab8352fda
Shunsuke Ito <sh-ito@sios.com>
2021-12-07 16:43:43 +0900

    i18n test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
+const { locales, defaultLocale } = require('./i18n.json')
+
 module.exports = {
-    i18n: {
-      locales: ['ja','en'],
-      defaultLocale: 'ja',
-    },
-  }
+  i18n: { locales, defaultLocale },
+}
